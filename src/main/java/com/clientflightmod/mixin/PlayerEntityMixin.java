5801044351f047bc140dcd5333d725f12511096b
这道题真的选C <104637904+ReallyChooseC@users.noreply.github.com>
2025-02-15 12:13:26 +0800

    fix

--- src/main/java/com/clientflightmod/mixin/PlayerEntityMixin.java
+++ src/main/java/com/clientflightmod/mixin/PlayerEntityMixin.java
@@ -4,7 +4,7 @@ import com.clientflightmod.ClientFlightMod;
 import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerEntity.class)
