402ce0fbec1d8e1f6950f43731af5f53f8d52689
这道题真的选C <104637904+ReallyChooseC@users.noreply.github.com>
2025-03-16 06:32:42 +0800

    最终修复

--- src/main/java/com/clientflightmod/ClientFlightMod.java
+++ src/main/java/com/clientflightmod/ClientFlightMod.java
@@ -1,41 +1,38 @@
 package com.clientflightmod;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.text.Text;
 
-import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.*;
-
 public class ClientFlightMod implements ClientModInitializer {
-    private static boolean flightEnabled = false;
-
+    
     @Override
     public void onInitializeClient() {
-		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
-    		dispatcher.register(literal("cfly")
-        		.executes(context -> {
-        		    flightEnabled = !flightEnabled;
-        		    updateFlightState();
-        		    context.getSource().sendFeedback(Text.literal("飞行模式 " + (flightEnabled ? "启用" : "禁用")));
-        		    return 1;
-    		    })
-		    );
-		});
+        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+            dispatcher.register(ClientCommandManager.literal("cfly")
+                .executes(context -> {
+                    MinecraftClient client = MinecraftClient.getInstance();
+                    if (client.player != null) {
+                        boolean canFly = client.player.getAbilities().allowFlying;
+                        
+                        client.player.getAbilities().allowFlying = !canFly;
+                        client.player.getAbilities().flying = !canFly;
+                        
+                        // 使用语言文件键
+                        String translationKey = canFly ? 
+                            "clientflightmod.fly.disabled" : 
+                            "clientflightmod.fly.enabled";
+                        
+                        client.player.sendMessage(
+                            Text.translatable(translationKey), 
+                            true
+                        );
+                    }
+                    return 1;
+                })
+            );
+        });
     }
-
-    private static void updateFlightState() {
-        ClientPlayerEntity player = MinecraftClient.getInstance().player;
-        if (player != null) {
-            player.getAbilities().allowFlying = flightEnabled;
-            if (!flightEnabled) {
-                player.getAbilities().flying = false;
-            }
-        }
-    }
-
-	public static boolean isFlightEnabled() {
-    	return flightEnabled;
-	}
 }
