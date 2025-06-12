307865fd7ce4ae4a20e276dd601abb90d7dd67c0
ChooseC <3464861631@qq.com>
2025-06-12 19:41:47 +0800

    1.1.6

--- src/main/java/com/clientflightmod/Nofall.java
+++ src/main/java/com/clientflightmod/Nofall.java
@@ -2,10 +2,10 @@ package com.clientflightmod;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.item.Items;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 import net.minecraft.text.Text;
 
-import static com.clientflightmod.ClientFlightMod.nofallDistance;
 import static com.clientflightmod.ClientFlightMod.nofallToggle;
 import static com.clientflightmod.Config.saveConfig;
 import static com.clientflightmod.Feedback.*;
@@ -14,12 +14,9 @@ public class Nofall {
     static void NofallDamage(MinecraftClient client) {
         ClientPlayerEntity player = client.player;
         if (player == null) return;
-
-        if (nofallToggle|!player.isGliding()) {
-            if(player.fallDistance > nofallDistance) {
+        if (nofallToggle&&!player.isGliding()&&!player.getMainHandStack().isOf(Items.MACE)) {
                 player.networkHandler
                         .sendPacket(new PlayerMoveC2SPacket.OnGroundOnly(true, player.horizontalCollision));
-            }
         }
     }
 
@@ -30,12 +27,6 @@ public class Nofall {
                 .append(Text.translatable(": "))
                 .append(Text.translatable("clientflightmod." + (nofallToggle ? "enabled" : "disabled")));
         sendCustomFeedback(message);
-    }
-
-    static void setNofallDistance(double value) {
-        nofallDistance = Math.max(0, value);
-        saveConfig();
-        sendFeedback("clientflightmod.nofallDistance_set", nofallDistance);
     }
 
 }
