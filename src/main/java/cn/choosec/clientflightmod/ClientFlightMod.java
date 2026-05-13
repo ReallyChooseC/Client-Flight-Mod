aaed6cd625b3755aca1d20454970958a7ccd9581
ChooseC <3464861631@qq.com>
2026-05-13 17:55:38 +0800

    26.1兼容

--- src/main/java/cn/choosec/clientflightmod/ClientFlightMod.java
+++ src/main/java/cn/choosec/clientflightmod/ClientFlightMod.java
@@ -3,10 +3,18 @@ package cn.choosec.clientflightmod;
 import com.mojang.blaze3d.platform.InputConstants;
 import com.mojang.brigadier.arguments.DoubleArgumentType;
 import net.fabricmc.api.ClientModInitializer;
+//#if MC>=260000
+//$$ import net.fabricmc.fabric.api.client.command.v2.ClientCommands;
+//#else
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+//#endif
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+//#if MC>=260000
+//$$ import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
+//#else
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+//#endif
 import net.minecraft.client.KeyMapping;
 import org.lwjgl.glfw.GLFW;
 //#if MC>=12109
@@ -49,13 +57,28 @@ public class ClientFlightMod implements ClientModInitializer {
         //#else
         //$$ Category clientflightmodCategory = KeyMapping.Category
         //$$         .register(ResourceLocation.fromNamespaceAndPath("clientflightmod", "main"));
+        //$$ //#if MC>=260000
+        //$$ //$$ flyKey = KeyMappingHelper.registerKeyMapping(new KeyMapping(
+        //$$ //#else
         //$$ flyKey = KeyBindingHelper.registerKeyBinding(new KeyMapping(
+        //$$ //#endif
         //$$         "key.clientflightmod.toggleflight",
         //$$         InputConstants.Type.KEYSYM,
         //$$         GLFW.GLFW_KEY_UNKNOWN,
         //$$         clientflightmodCategory));
         //#endif
 
+        //#if MC>=260000
+        //$$ ClientCommandRegistrationCallback.EVENT.register((dispatcher, buildContext) -> dispatcher.register(ClientCommands.literal("cfly")
+        //$$         .then(ClientCommands.literal("toggle").executes(ctx -> { toggleFlight(); return 1; }))
+        //$$         .then(ClientCommands.literal("elytratoggle").executes(ctx -> { toggleElytra(); return 1; }))
+        //$$         .then(ClientCommands.literal("nofalltoggle").executes(ctx -> { toggleNofall(); return 1; }))
+        //$$         .then(ClientCommands.literal("forceflighttoggle").executes(ctx -> { toggleForceFlight(); return 1; }))
+        //$$         .then(ClientCommands.literal("speed")
+        //$$                 .then(ClientCommands.argument("value", DoubleArgumentType.doubleArg(0.0))
+        //$$                         .executes(ctx -> { setSpeed(DoubleArgumentType.getDouble(ctx, "value")); return 1; }))
+        //$$         )));
+        //#else
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> dispatcher.register(ClientCommandManager.literal("cfly")
                 .then(ClientCommandManager.literal("toggle").executes(ctx -> { toggleFlight(); return 1; }))
                 .then(ClientCommandManager.literal("elytratoggle").executes(ctx -> { toggleElytra(); return 1; }))
@@ -65,10 +88,11 @@ public class ClientFlightMod implements ClientModInitializer {
                         .then(ClientCommandManager.argument("value", DoubleArgumentType.doubleArg(0.0))
                                 .executes(ctx -> { setSpeed(DoubleArgumentType.getDouble(ctx, "value")); return 1; }))
                 )));
+        //#endif
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             if (flyKey.consumeClick()) toggleFlight();
-            if (nofallToggle) noFallDamage(client);;
+            if (nofallToggle) noFallDamage(client);
             if (forceflightToggle) forceFlight();
         });
     }
--- src/main/java/cn/choosec/clientflightmod/Feedback.java
+++ src/main/java/cn/choosec/clientflightmod/Feedback.java
@@ -7,14 +7,22 @@ public class Feedback {
     static void sendFeedback(String key, Object... args) {
         Minecraft client = Minecraft.getInstance();
         if (client.player != null) {
+            //#if MC>=260000
+            //$$ client.player.sendOverlayMessage(Component.translatable(key, args));
+            //#else
             client.player.displayClientMessage(Component.translatable(key, args), true);
+            //#endif
         }
     }
 
     static void sendCustomFeedback(Component message) {
         Minecraft client = Minecraft.getInstance();
         if (client.player != null) {
+            //#if MC>=260000
+            //$$ client.player.sendOverlayMessage(message);
+            //#else
             client.player.displayClientMessage(message, true);
+            //#endif
         }
     }
 }
